0c737d22878e3054b2fdc7ae18f539d8b8844d89
SungJun Hong <castle_jun@daum.net>
2026-06-19 11:35:35 +0900

    initial commit

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 6.3
+
+import PackageDescription
+
+let package = Package(
+  name: "SteadyUI",
+  products: [
+    .library(
+      name: "SteadyUI",
+      targets: ["SteadyUI"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "SteadyUI"
+    ),
+  ],
+  swiftLanguageModes: [.v6]
+)
