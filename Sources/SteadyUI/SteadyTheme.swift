af67684fbcfec02610f155889601e413c18036e1
SungJun Hong <castle_jun@daum.net>
2026-06-19 14:13:35 +0900

    Add spacing and radius tokens

--- Sources/SteadyUI/SteadyTheme.swift
+++ Sources/SteadyUI/SteadyTheme.swift
@@ -3,12 +3,18 @@ import Foundation
 public struct SteadyTheme: Equatable, Sendable {
   public var colors: ColorTokens
   public var fonts: FontTokens
+  public var radius: RadiusTokens
+  public var spacing: SpacingTokens
 
   public init(
     colors: ColorTokens,
-    fonts: FontTokens
+    fonts: FontTokens,
+    radius: RadiusTokens,
+    spacing: SpacingTokens
   ) {
     self.colors = colors
     self.fonts = fonts
+    self.radius = radius
+    self.spacing = spacing
   }
 }
